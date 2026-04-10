25ee9bd477c79c0223f47956fbfa06654267264c
Mohd Farhan <165193271+farhan4783@users.noreply.github.com>
2026-04-10 14:00:40 +0530

    Implement reverse greedy approach for canJump

--- jump.java
+++ jump.java
@@ -23,3 +23,22 @@ class Solution {
         return true;
     }
 }
+
+
+
+//reverse greedy 
+
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;
+    }
+}
