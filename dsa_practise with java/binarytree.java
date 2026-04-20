901c4026810855571aebf6d0623eb3b374f39aa1
Mohd Farhan <165193271+farhan4783@users.noreply.github.com>
2026-04-20 23:38:26 +0530

    Implement iterative searchBST method in Solution class
    Added an iterative method to search in the binary search tree.

--- dsa_practise with java/binarytree.java
+++ dsa_practise with java/binarytree.java
@@ -31,3 +31,15 @@ class Solution {
         return searchBST(root.right, val);
     }
 }
+
+
+//iterative
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while (root != null && root.val != val) {
+            root = (val < root.val) ? root.left : root.right;
+        }
+        return root;
+    }
+}
