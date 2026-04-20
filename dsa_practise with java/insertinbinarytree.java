4c2802768d175c2a125ceb48eaf48e3cbfa3aa72
Mohd Farhan <165193271+farhan4783@users.noreply.github.com>
2026-04-20 23:45:05 +0530

    Add iterative method to insert into BST

--- dsa_practise with java/insertinbinarytree.java
+++ dsa_practise with java/insertinbinarytree.java
@@ -17,3 +17,30 @@ class Solution {
         return root;
     }
 }
+
+//iterative
+
+
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        
+        TreeNode curr = root;
+        while (true) {
+            if (val > curr.val) {
+                if (curr.right == null) {
+                    curr.right = new TreeNode(val);
+                    break;
+                }
+                curr = curr.right;
+            } else {
+                if (curr.left == null) {
+                    curr.left = new TreeNode(val);
+                    break;
+                }
+                curr = curr.left;
+            }
+        }
+        return root;
+    }
+}
