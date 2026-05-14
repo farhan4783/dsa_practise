165e37da4979fb68212ebaa4b3d34ac6841aa417
Mohd Farhan <165193271+farhan4783@users.noreply.github.com>
2026-05-14 12:11:27 +0530

    Add isGood method to check array conditions

---
+++ dsa_practise with java/arrayisgood.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isGood(int[] nums) {
+        int len = nums.length;
+        if (len < 2) return false; 
+
+        int n = len - 1;
+        int[] count = new int[len]; 
+
+        for (int num : nums) {
+            
+            if (num < 1 || num > n) {
+                return false;
+            }
+            count[num]++;
+        }
+
+      
+        for (int i = 1; i < n; i++) {
+            if (count[i] != 1) {
+                return false;
+            }
+        }
+
+        return count[n] == 2;
+    }
+}
